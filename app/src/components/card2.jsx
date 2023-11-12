8faebab3e1ef45e0322735bb5b116cf03ec6b011
Abdirazak Yusuf <abdirazak140@gmail.com>
2023-11-12 15:43:53 -0500

    Made basic dashboard

---
+++ app/src/components/card2.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Card2 = ({ children }) => {
+    return (
+        <div className="bg-white p-6 w-10/12 h-48 rounded-lg shadow-md">
+            {children}
+		</div>
+    );
+};
+
+export default Card2;
