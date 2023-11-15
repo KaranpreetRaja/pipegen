472711dac2707c513de69ace41ecc720c484ce06
Abdirazak Yusuf <abdirazak140@gmail.com>
2023-11-15 17:44:31 -0500

    Added chatbot path

---
+++ app/src/pages/ChatBot.jsx
@@ -0,0 +1,14 @@
+
+
+const ChatBot = () => {
+	return (
+        <div>
+            <Navbar/>
+            <div className="flex-1 flex flex-col overflow-hidden bg-gray-100 h-full">
+                
+            </div>
+        </div>
+    )
+}
+
+export default ChatBot;
