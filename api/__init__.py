e39faad222540931c5468fa251de2807bfe85774
Karanpreet Raja <86526687+KaranpreetRaja@users.noreply.github.com>
2023-11-14 22:11:12 -0500

    Fix flask app run

--- api/__init__.py
+++ api/__init__.py
@@ -1,12 +1,18 @@
+import os
 from flask import Flask
 from firebase_admin import credentials, firestore, initialize_app
-from .userAPI import user_api
 
-
-cred = credentials.Certificate("key.json")
+cur_path = os.path.dirname(os.path.realpath(__file__))
+cred = credentials.Certificate(f"{cur_path}/key.json")
 default_app = initialize_app(cred)
 
 def init_app():
     app = Flask(__name__)
-    app.register_blueprint(user_api, url_prefix='/api/user')
+    from userAPI import user_api
 
+    app.register_blueprint(user_api, url_prefix='/api/user')
+    
+    app.run(debug=True)
+
+if __name__ == '__main__':
+    init_app()
