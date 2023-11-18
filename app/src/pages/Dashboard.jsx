6e7331389047702efb14cdba7bcf214ea5abd9d1
Karanpreet Raja <86526687+KaranpreetRaja@users.noreply.github.com>
2023-11-18 09:50:50 -0500

    Update Dashboard.jsx

--- app/src/pages/Dashboard.jsx
+++ app/src/pages/Dashboard.jsx
@@ -1,76 +1,87 @@
-import React from 'react';
-import Navbar from '../components/Navbar';
-import Card1 from '../components/card1';
-import Card2 from '../components/card2';
-import { FaRocketchat } from 'react-icons/fa';
-import PipelineConfig from '../components/PipelineConfig';
-import Pipeline from '../components/Pipeline';
+import React from "react";
+import Navbar from "../components/Navbar";
+import Card1 from "../components/card1";
+import Card2 from "../components/card2";
+import { FaRocketchat } from "react-icons/fa";
+import PipelineConfig from "../components/PipelineConfig";
+import Pipeline from "../components/Pipeline";
 
 const Dashboard = () => {
-	return (
-		<>
-			<Navbar />
-			<div className="flex-1 flex flex-col overflow-hidden bg-gray-100 h-full">
-				<div className="container mx-auto mt-8 ml-8">
-					<div className="mb-4">
-						<h1 className="text-3xl font-bold mb-4">Dashboard</h1>
-						<h2 className="text-1xl font-semibold">Sessions</h2>
-						<div className="flex card-scroll">
-							<Card1 />
-							<Card1 />
-							<Card1 />
-							<Card1 />
-						</div>
-						<h2 className="text-1xl font-semibold mt-2">Recently Made Pipelines</h2>
-						<div className="flex flex-row w-full card-scroll">
-							<Pipeline />
-							<Pipeline />
-							<Pipeline />
-							<Pipeline />
-							<Pipeline />
-							<Pipeline />
-							<Pipeline />
-							<Pipeline />
-							<Pipeline />
-							<Pipeline />
-							<Pipeline />
-			<div className="flex flex-col w-full justify-center items-center">
-				<div className="flex-1 flex flex-col overflow-hidden h-full">
-					<div className="container w-full mx-auto mt-8 ml-8">
-						<div className="mb-4 w-full">
-							<h1 className="text-3xl font-bold mb-4">Dashboard</h1>
-							<h2 className="text-1xl font-semibold">Sessions</h2>
-							<div className="flex card-scroll">
-								<Card1 />
-								<Card1 />
-								<Card1 />
-								<Card1 />
-							</div>
-							<h2 className="text-1xl font-semibold mt-2">Recent Pipelines Pipeline</h2>
-							<div className="flex card-scroll">
-								<Card1 />
-								<Card1 />
-								<Card1 />
-								<Card1 />
-							</div>
-						</div>
-						<div className='flex flex-row justify-center items-center mb-6'>
-							<div className="bg-white h-68 max-w-lg p-12 w-full  rounded-lg flex flex-col items-center">
-								<div className="text-4xl text-blue-500 mb-4 mt-5">
-									<FaRocketchat />
-								</div>
-								<h2 className="text-xl font-semibold mb-2">Create a New Pipeline</h2>
-								<p className="text-gray-600 mb-4 center text-center">
-									Click the button below to start your new Pipeline Session
-								</p>
-								<PipelineConfig />
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <Navbar />
+      {/* <div className="flex-1 flex flex-col overflow-hidden bg-gray-100 h-full">
+        <div className="container mx-auto mt-8 ml-8">
+          <div className="mb-4">
+            <h1 className="text-3xl font-bold mb-4">Dashboard</h1>
+            <h2 className="text-1xl font-semibold">Sessions</h2>
+            <div className="flex card-scroll">
+              <Card1 />
+              <Card1 />
+              <Card1 />
+              <Card1 />
+            </div>
+            <h2 className="text-1xl font-semibold mt-2">
+              Recently Made Pipelines
+            </h2>
+            <div className="flex flex-row w-full card-scroll">
+              <Pipeline />
+              <Pipeline />
+              <Pipeline />
+              <Pipeline />
+              <Pipeline />
+              <Pipeline />
+              <Pipeline />
+              <Pipeline />
+              <Pipeline />
+              <Pipeline />
+              <Pipeline />
+            </div>
+          </div>
+        </div>
+      </div> */}
+
+      <div className="flex flex-col w-full justify-center items-center">
+        <div className="flex-1 flex flex-col overflow-hidden h-full">
+          <div className="container w-full mx-auto mt-8 ml-8">
+            <div className="mb-4 w-full">
+              <h1 className="text-3xl font-bold mb-4">Dashboard</h1>
+              <h2 className="text-1xl font-semibold">Sessions</h2>
+              <div className="flex card-scroll">
+                <Card1 />
+                <Card1 />
+                <Card1 />
+                <Card1 />
+              </div>
+              <h2 className="text-1xl font-semibold mt-2">
+                Recent Pipelines Pipeline
+              </h2>
+              <div className="flex card-scroll">
+                <Card1 />
+                <Card1 />
+                <Card1 />
+                <Card1 />
+              </div>
+            </div>
+            <div className="flex flex-row justify-center items-center mb-6">
+              <div className="bg-white h-68 max-w-lg p-12 w-full  rounded-lg flex flex-col items-center">
+                <div className="text-4xl text-blue-500 mb-4 mt-5">
+                  <FaRocketchat />
+                </div>
+                <h2 className="text-xl font-semibold mb-2">
+                  Create a New Pipeline
+                </h2>
+                <p className="text-gray-600 mb-4 center text-center">
+                  Click the button below to start your new Pipeline Session
+                </p>
+                <PipelineConfig />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Dashboard;
